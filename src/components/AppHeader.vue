812963c3d5ef7b4ca6c8b0f5b0c8ba1e11ba06f4
Anieb22 <anielloverdino@gmail.com>
2023-06-27 08:36:53 +0200

    creata struttura grafica header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+
+}
+</script>
+
+<template lang="">
+  <header>
+    <div class="container">
+      <div class="row align-items-center justify-content-between d-flex">
+        <div class="col-3">
+          <img src="../assets/img/logo-boolflix@4x.png" alt="" class="w-75">
+        </div>
+        <div class="col-3">
+          <div class="input-group input-group-sm">
+            <input type="text" class="form-control" placeholder="Search Movie" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<style lang="scss" scoped>
+header {
+  background-color: rgb(0, 0, 0);
+}
+</style>
