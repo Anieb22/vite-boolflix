d9409fa98a50244036c1dd64316b7b4e2b40bffd
Anieb22 <anielloverdino@gmail.com>
2023-06-27 17:06:29 +0200

    definito funzionamento serachbar da plaseholders

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    movieApi: 'https://api.themoviedb.org/3/movie/550?api_key=aa96e5802311da937f2a7065ffd89de9',
-    myMovie: []
+    movieApi: 'https://api.themoviedb.org/3/search/movie?api_key=aa96e5802311da937f2a7065ffd89de9&query=',
+    searchMovie: '',
 })
